b76a24893fd4f6b9ea5f4a6536b048c3f58ee1e9
James Robert <jiaaro@gmail.com>
2014-06-19 10:13:21 -0400

    Initial Commit

---
+++ Grocery Seer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grocery Seer
+//
+//  Created by James Robert on 6/19/14.
+//  Copyright (c) 2014 Jiaaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
