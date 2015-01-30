e2ffc53cacb8e6cb7fcc92892101da7f0f8deea5
James Robert <jiaaro@gmail.com>
2015-01-29 21:44:19 -0500

    add watch icons and user pre-rendered checkboxes

--- Daily Bread WatchKit Extension/InterfaceController.swift
+++ Daily Bread WatchKit Extension/InterfaceController.swift
@@ -21,21 +21,36 @@ class Grocery {
     }
 }
 
+let empty_list_message = Grocery(["id": "0", "name": "No Data", "bought": "no"])
+
 class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var groceryTable: WKInterfaceTable!
     
-    var needs_refresh = true
+    var needs_ui_refresh = true
+//    
+//    let cb_unchecked: UIImage = StyleKit.imageOfCheckbox(isChecked: false)
+//    let cb_checked: UIImage = StyleKit.imageOfCheckbox(isChecked: true)
     
-    let cb_unchecked: UIImage = StyleKit.imageOfCheckbox(isChecked: false)
-    let cb_checked: UIImage = StyleKit.imageOfCheckbox(isChecked: true)
     var groceries: [Grocery] = [
-        Grocery(["id": "0", "name": "No Data", "bought": "no"]),
+        empty_list_message,
     ]
     
     override init() {
         super.init()
         println("init")
+        
+        self.refresh_data()
+    }
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        self.refresh_ui()
+    }
+    @IBAction func menuItemRefresh() {
+        self.refresh_data()
+    }
+    func refresh_data() {
         WKInterfaceController.openParentApplication([:]) {
             [weak self] (replyInfo, error) in
             
@@ -47,39 +62,48 @@ class InterfaceController: WKInterfaceController {
             self?.groceries = map(grocery_info_list) {
                 Grocery($0)
             }
-            self?.needs_refresh = true
-            self?.refresh_data()
+            
+            if self?.groceries.count == 0 {
+                self?.groceries = [empty_list_message]
+            }
+            
+            self?.needs_ui_refresh = true
+            self?.refresh_ui()
             return
         }
     }
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        self.refresh_data()
-    }
-    
-    func refresh_data() {
+    func refresh_ui() {
         println("refresh_data")
-        if !self.needs_refresh {
+        if !self.needs_ui_refresh {
             return
         }
-        self.needs_refresh = false
+        self.needs_ui_refresh = false
         
         let grocery_count = countElements(self.groceries)
+        let row_difference = grocery_count - groceryTable.numberOfRows
         
-        groceryTable.setNumberOfRows(grocery_count, withRowType: "groceryRow")
+        if row_difference > 0 {
+            let indexes = NSIndexSet(indexesInRange: NSRange(0..<row_difference))
+            groceryTable.insertRowsAtIndexes(indexes, withRowType: "groceryRow")
+        }
+        if row_difference < 0 {
+            let indexes = NSIndexSet(indexesInRange: NSRange(0..<(-row_difference)))
+            groceryTable.removeRowsAtIndexes(indexes)
+        }
+        //groceryTable.setNumberOfRows(grocery_count, withRowType: "groceryRow")
         
         for i in 0..<grocery_count {
             let row = groceryTable.rowControllerAtIndex(i) as groceryRowController
             let grocery = self.groceries[i]
             
+            println("setting text: \(grocery.name)")
             row.label.setText(grocery.name)
             
             if grocery.bought {
-                row.img.setImage(cb_checked)
+                row.img.setImageNamed("checked_box")
             }
             else {
-                row.img.setImage(cb_unchecked)
+                row.img.setImageNamed("unchecked_box")
             }
         }
     }
@@ -90,17 +114,17 @@ class InterfaceController: WKInterfaceController {
         
         let row = table.rowControllerAtIndex(rowIndex) as groceryRowController
         if grocery.bought {
-            row.img.setImage(cb_checked)
+            row.img.setImageNamed("checked_box")
         }
         else {
-            row.img.setImage(cb_unchecked)
+            row.img.setImageNamed("unchecked_box")
         }
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        self.refresh_data()
+        self.refresh_ui()
     }
 
     override func didDeactivate() {
